1a16b3dc2360862de5465301f360e8d81fa809ba
Christian <chris_veenman@hotmail.com>
2019-01-27 20:43:00 +0100

    Updated the MRTReader to return an Option when there are no more entries to read. Updated the tests accordingly using BufReader.

--- tests/ripe.rs
+++ tests/ripe.rs
@@ -2,56 +2,44 @@ use libflate::gzip::Decoder;
 use mrt_rs::MRTReader;
 use mrt_rs::MRTRecord;
 use mrt_rs::BGP4MP;
+use mrt_rs::TABLE_DUMP_V2;
 use std::fs::File;
-use std::io::Cursor;
 use std::io::Read;
+use std::io::BufReader;
 
 // Tests if it is able to parse a stream of BGP4MP messages.
 #[test]
 fn parse_updates() {
-    // Download an update message
+    // Download an update message.
     let file = File::open("res/updates.20190101.0000.gz").unwrap();
 
-    // Decode the GZIP stream
-    let mut decoder = Decoder::new(file).unwrap();
+    // Decode the GZIP stream.
+    let mut decoder = Decoder::new(BufReader::new(file)).unwrap();
 
-    // Parse the contents
-    parse(&mut decoder)
+    // Create a new MRTReader with a Cursor such that we can keep track of the position.
+    let mut reader = MRTReader {stream: decoder};
+
+    while let Ok(Some(record)) = reader.read() {
+        match record {
+            _ => continue,
+        }
+    }
 }
 
 // Tests if it is able to parse a stream of TABLE_DUMP_V2 messages.
 #[test]
 fn parse_rib() {
-    // Download an update message
+    // Download an update message.
     let file = File::open("res/bview.20100101.0759.gz").unwrap();
 
-    // Decode the GZIP stream
-    let mut decoder = Decoder::new(file).unwrap();
+    // Decode the GZIP stream.
+    let mut decoder = Decoder::new(BufReader::new(file)).unwrap();
 
-    // Parse the contents
-    parse(&mut decoder)
-}
+    // Create a new MRTReader
+    let mut reader = MRTReader { stream: decoder };
 
-fn parse(readable: &mut Read) {
-    // Read the entire contents of the File in a buffer.
-    let mut buffer: Vec<u8> = vec![];
-    readable.read_to_end(&mut buffer).unwrap();
-    let length = buffer.len() as u64;
-
-    // Create a new MRTReader with a Cursor such that we can keep track of the position.
-    let mut reader = MRTReader {
-        stream: Cursor::new(buffer),
-    };
-
-    // Keep reading entries till
-    while reader.stream.position() < length {
-        let result = reader.read();
-        match &result.unwrap() {
-            MRTRecord::BGP4MP(x) => match x {
-                BGP4MP::MESSAGE(y) => println!("{:?}", y),
-                BGP4MP::MESSAGE_AS4(y) => println!("{:?}", y),
-                _ => continue,
-            },
+    while let Ok(Some(record)) = reader.read() {
+        match record {
             _ => continue,
         }
     }
